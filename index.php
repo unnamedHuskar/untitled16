5b32eaad784afe219dcb90cf0f3dca23fcf89021
alevur <alevure@yandex.ru>
2023-05-08 20:46:39 +0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Главная</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <a href="#"><img class="logo" src="./assets/img/logo.jpg" alt="logo"/></a>
+            <form method="get">
+                <input type="text" placeholder="Поиск">
+                <input type="submit" value="Найти">
+            </form>
+            <a href="./pages/login.php"><img class="auth" src="./assets/img/png-clipart-car-lada-priora-vaz-2101-sticker-car-compact-car-glass.png" alt="avtorise"/></a>
+        </div>
+
+
+    </header>
+</body>
+</html>
---
+++ pages/login.php
@@ -0,0 +1,10 @@
+<head>
+    <title>Авторизация</title>
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" placeholder="Введите логин:" name="login">
+        <input type="text" placeholder="Введите пароль:" name="password">
+    </form>
+</body>
